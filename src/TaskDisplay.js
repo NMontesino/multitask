645504e33796b8eac9e2e82ffe31a27929ae7eba
Nick Montesino <namontesino@gmail.com>
2020-11-25 11:50:15 -0500

    Bare bones file and layout steup

---
+++ src/TaskDisplay.js
@@ -0,0 +1,14 @@
+import './TaskDisplay.css'
+
+const TaskDisplay = () => 
+{
+
+  return (
+    <div>
+      
+    </div>
+  )
+  
+}
+
+export default TaskDisplay
---
+++ src/TaskInput.js
@@ -0,0 +1,16 @@
+import './TaskInput.css'
+
+const TaskInput = () => 
+{
+
+  return (
+    <div>
+      <form>
+          <input type="text" />
+      </form>
+    </div>
+  )
+  
+}
+
+export default TaskInput
